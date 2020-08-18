c053f8085c3055ba93eb26af1d7726d20df0546c
Juan Medina <jamedina@gmail.com>
2020-08-18 17:10:53 +0100

    adding colors

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -48,25 +48,29 @@ const (
 	stickToBottom
 )
 
-type stickText struct {
+type stickyText struct {
 	size    float64
 	spacing float64
 	stick   stickPosition
 }
 
-var stickTextType = reflect.TypeOf(stickText{})
+var stickyTextType = reflect.TypeOf(stickyText{})
 
 type stickyTextSystem struct{}
+
+func (cts stickyTextSystem) Update(_ *world.World, _ float64) error {
+	return nil
+}
 
 func (cts stickyTextSystem) Notify(wld *world.World, event interface{}, _ float64) error {
 	switch e := event.(type) {
 	case events.ScreenSizeChangeEvent:
 		// get all our texts
-		for _, v := range wld.Entities(components.PosType, components.UiTextType, stickTextType) {
+		for _, v := range wld.Entities(components.PosType, components.UiTextType, stickyTextType) {
 			// get the text components
 			pos := v.Get(components.PosType).(components.Pos)
 			text := v.Get(components.UiTextType).(components.UiText)
-			st := v.Get(stickTextType).(stickText)
+			st := v.Get(stickyTextType).(stickyText)
 
 			// calculate position based on current screen size and stick
 			switch st.stick {
@@ -89,22 +93,74 @@ func (cts stickyTextSystem) Notify(wld *world.World, event interface{}, _ float6
 	return nil
 }
 
-func (cts stickyTextSystem) Update(_ *world.World, _ float64) error {
+type alternateColor struct {
+	from    color.Color
+	to      color.Color
+	time    float64
+	current float64
+}
+
+var alternateColorType = reflect.TypeOf(alternateColor{})
+
+type alternateColorSystem struct{}
+
+func (rcs alternateColorSystem) Update(world *world.World, delta float64) error {
+	for _, v := range world.Entities(components.ColorType, alternateColorType) {
+		clr := v.Get(components.ColorType).(color.Color)
+		ac := v.Get(alternateColorType).(alternateColor)
+
+		r1, g1, b1, a1 := ac.from.RGBA()
+		r2, g2, b2, a2 := ac.to.RGBA()
+		s := ac.current / ac.time
+
+		r := float64(uint8(r1)) + (float64(uint8(r2))-float64(uint8(r1)))*s
+		g := float64(uint8(g1)) + (float64(uint8(g2))-float64(uint8(g1)))*s
+		b := float64(uint8(b1)) + (float64(uint8(b2))-float64(uint8(b1)))*s
+		a := float64(uint8(a1)) + (float64(uint8(a2))-float64(uint8(a1)))*s
+
+		clr = color.RGBA{
+			R: uint8(r),
+			G: uint8(g),
+			B: uint8(b),
+			A: uint8(a),
+		}
+
+		ac.current += delta
+
+		if ac.current > ac.time {
+			ac.current = 0
+			aux := ac.from
+			ac.from = ac.to
+			ac.to = aux
+		}
+
+		v.Set(clr)
+		v.Set(ac)
+	}
+	return nil
+}
+
+func (rcs alternateColorSystem) Notify(_ *world.World, _ interface{}, _ float64) error {
 	return nil
 }
 
 func loadGame(gWorld *world.World) error {
 	gWorld.Add(entitiy.New(
 		components.UiText{
-			String:     "Hello world",
+			String:     "Hello World",
 			Size:       300,
 			Spacing:    10,
 			HAlignment: components.CenterHAlignment,
 			VAlignment: components.MiddleVAlignment,
 		},
-		stickText{size: 300, spacing: 10, stick: stickToCenter},
 		components.Pos{X: float64(opt.Width / 2), Y: float64(opt.Height / 2)},
 		color.RGBA{R: 255, G: 255, B: 255, A: 255},
+		stickyText{size: 300, spacing: 10, stick: stickToCenter},
+		alternateColor{
+			time: 1,
+			from: color.RGBA{R: 0, G: 255, B: 255, A: 255},
+			to:   color.RGBA{R: 255, G: 0, B: 0, A: 255},
+		},
 	))
 	gWorld.Add(entitiy.New(
 		components.UiText{
@@ -114,11 +170,17 @@ func loadGame(gWorld *world.World) error {
 			HAlignment: components.CenterHAlignment,
 			VAlignment: components.BottomVAlignment,
 		},
-		stickText{size: 60, spacing: 10, stick: stickToBottom},
 		components.Pos{X: float64(opt.Width / 2), Y: float64(opt.Height)},
 		color.RGBA{R: 255, G: 255, B: 255, A: 255},
+		stickyText{size: 60, spacing: 10, stick: stickToBottom},
+		alternateColor{
+			time: .25,
+			from: color.RGBA{R: 255, G: 255, B: 255, A: 255},
+			to:   color.RGBA{R: 255, G: 255, B: 255, A: 0},
+		},
 	))
 	gWorld.AddSystem(stickyTextSystem{})
+	gWorld.AddSystem(alternateColorSystem{})
 	return nil
 }
 
