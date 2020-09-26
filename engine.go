389b035fdde3050b9ada8a3c72dae64a9be95c64
Juan Medina <jamedina@gmail.com>
2020-09-26 09:45:01 +0100

    v0.1.10

--- engine.go
+++ engine.go
@@ -50,7 +50,7 @@ const (
 	lastPriority = int32(-1000)
 	lowPriority  = int32(-500)
 	highPriority = int32(500)
-	gosgeVersion = "v0.1.9"
+	gosgeVersion = "v0.1.10"
 )
 
 // InitFunc is a function that will get call for our game to load
